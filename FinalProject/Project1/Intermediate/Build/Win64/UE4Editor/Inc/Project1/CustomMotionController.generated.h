a844453958c6a34eff7383f18fd89730d0c0186b
yjlo <vhv1122@gmail.com>
2019-04-30 17:26:20 -0400

    added final project

---
+++ FinalProject/Project1/Intermediate/Build/Win64/UE4Editor/Inc/Project1/CustomMotionController.generated.h
@@ -0,0 +1,147 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+struct FTeleportDestinationTransform;
+class AActor;
+#ifdef PROJECT1_CustomMotionController_generated_h
+#error "CustomMotionController.generated.h already included, missing '#pragma once' in CustomMotionController.h"
+#endif
+#define PROJECT1_CustomMotionController_generated_h
+
+#define Project1_Source_Project1_CustomMotionController_h_15_GENERATED_BODY \
+	friend struct Z_Construct_UScriptStruct_FTeleportDestinationTransform_Statics; \
+	PROJECT1_API static class UScriptStruct* StaticStruct();
+
+
+#define Project1_Source_Project1_CustomMotionController_h_26_RPC_WRAPPERS \
+ \
+	DECLARE_FUNCTION(execGetTeleportDestination) \
+	{ \
+		P_FINISH; \
+		P_NATIVE_BEGIN; \
+		*(FTeleportDestinationTransform*)Z_Param__Result=P_THIS->GetTeleportDestination(); \
+		P_NATIVE_END; \
+	} \
+ \
+	DECLARE_FUNCTION(execGetActorNearHand) \
+	{ \
+		P_FINISH; \
+		P_NATIVE_BEGIN; \
+		*(AActor**)Z_Param__Result=P_THIS->GetActorNearHand(); \
+		P_NATIVE_END; \
+	} \
+ \
+	DECLARE_FUNCTION(execClearArc) \
+	{ \
+		P_FINISH; \
+		P_NATIVE_BEGIN; \
+		P_THIS->ClearArc(); \
+		P_NATIVE_END; \
+	}
+
+
+#define Project1_Source_Project1_CustomMotionController_h_26_RPC_WRAPPERS_NO_PURE_DECLS \
+ \
+	DECLARE_FUNCTION(execGetTeleportDestination) \
+	{ \
+		P_FINISH; \
+		P_NATIVE_BEGIN; \
+		*(FTeleportDestinationTransform*)Z_Param__Result=P_THIS->GetTeleportDestination(); \
+		P_NATIVE_END; \
+	} \
+ \
+	DECLARE_FUNCTION(execGetActorNearHand) \
+	{ \
+		P_FINISH; \
+		P_NATIVE_BEGIN; \
+		*(AActor**)Z_Param__Result=P_THIS->GetActorNearHand(); \
+		P_NATIVE_END; \
+	} \
+ \
+	DECLARE_FUNCTION(execClearArc) \
+	{ \
+		P_FINISH; \
+		P_NATIVE_BEGIN; \
+		P_THIS->ClearArc(); \
+		P_NATIVE_END; \
+	}
+
+
+#define Project1_Source_Project1_CustomMotionController_h_26_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesACustomMotionController(); \
+	friend struct Z_Construct_UClass_ACustomMotionController_Statics; \
+public: \
+	DECLARE_CLASS(ACustomMotionController, AActor, COMPILED_IN_FLAGS(CLASS_Abstract), CASTCLASS_None, TEXT("/Script/Project1"), NO_API) \
+	DECLARE_SERIALIZER(ACustomMotionController)
+
+
+#define Project1_Source_Project1_CustomMotionController_h_26_INCLASS \
+private: \
+	static void StaticRegisterNativesACustomMotionController(); \
+	friend struct Z_Construct_UClass_ACustomMotionController_Statics; \
+public: \
+	DECLARE_CLASS(ACustomMotionController, AActor, COMPILED_IN_FLAGS(CLASS_Abstract), CASTCLASS_None, TEXT("/Script/Project1"), NO_API) \
+	DECLARE_SERIALIZER(ACustomMotionController)
+
+
+#define Project1_Source_Project1_CustomMotionController_h_26_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ACustomMotionController(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACustomMotionController) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACustomMotionController); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACustomMotionController); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACustomMotionController(ACustomMotionController&&); \
+	NO_API ACustomMotionController(const ACustomMotionController&); \
+public:
+
+
+#define Project1_Source_Project1_CustomMotionController_h_26_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACustomMotionController(ACustomMotionController&&); \
+	NO_API ACustomMotionController(const ACustomMotionController&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACustomMotionController); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACustomMotionController); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ACustomMotionController)
+
+
+#define Project1_Source_Project1_CustomMotionController_h_26_PRIVATE_PROPERTY_OFFSET
+#define Project1_Source_Project1_CustomMotionController_h_23_PROLOG
+#define Project1_Source_Project1_CustomMotionController_h_26_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Project1_Source_Project1_CustomMotionController_h_26_PRIVATE_PROPERTY_OFFSET \
+	Project1_Source_Project1_CustomMotionController_h_26_RPC_WRAPPERS \
+	Project1_Source_Project1_CustomMotionController_h_26_INCLASS \
+	Project1_Source_Project1_CustomMotionController_h_26_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Project1_Source_Project1_CustomMotionController_h_26_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Project1_Source_Project1_CustomMotionController_h_26_PRIVATE_PROPERTY_OFFSET \
+	Project1_Source_Project1_CustomMotionController_h_26_RPC_WRAPPERS_NO_PURE_DECLS \
+	Project1_Source_Project1_CustomMotionController_h_26_INCLASS_NO_PURE_DECLS \
+	Project1_Source_Project1_CustomMotionController_h_26_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Project1_Source_Project1_CustomMotionController_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ FinalProject/Project1/Intermediate/Build/Win64/UE4Editor/Inc/Project1/MyPawn.gen.cpp
@@ -0,0 +1,109 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "Project1/MyPawn.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeMyPawn() {}
+// Cross Module References
+	PROJECT1_API UClass* Z_Construct_UClass_AMyPawn_NoRegister();
+	PROJECT1_API UClass* Z_Construct_UClass_AMyPawn();
+	ENGINE_API UClass* Z_Construct_UClass_APawn();
+	UPackage* Z_Construct_UPackage__Script_Project1();
+	PROJECT1_API UFunction* Z_Construct_UFunction_AMyPawn_JumpTeleportMoveForward();
+// End Cross Module References
+	void AMyPawn::StaticRegisterNativesAMyPawn()
+	{
+		UClass* Class = AMyPawn::StaticClass();
+		static const FNameNativePtrPair Funcs[] = {
+			{ "JumpTeleportMoveForward", &AMyPawn::execJumpTeleportMoveForward },
+		};
+		FNativeFunctionRegistrar::RegisterFunctions(Class, Funcs, ARRAY_COUNT(Funcs));
+	}
+	struct Z_Construct_UFunction_AMyPawn_JumpTeleportMoveForward_Statics
+	{
+#if WITH_METADATA
+		static const UE4CodeGen_Private::FMetaDataPairParam Function_MetaDataParams[];
+#endif
+		static const UE4CodeGen_Private::FFunctionParams FuncParams;
+	};
+#if WITH_METADATA
+	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UFunction_AMyPawn_JumpTeleportMoveForward_Statics::Function_MetaDataParams[] = {
+		{ "ModuleRelativePath", "MyPawn.h" },
+	};
+#endif
+	const UE4CodeGen_Private::FFunctionParams Z_Construct_UFunction_AMyPawn_JumpTeleportMoveForward_Statics::FuncParams = { (UObject*(*)())Z_Construct_UClass_AMyPawn, "JumpTeleportMoveForward", RF_Public|RF_Transient|RF_MarkAsNative, nullptr, (EFunctionFlags)0x00020401, 0, nullptr, 0, 0, 0, METADATA_PARAMS(Z_Construct_UFunction_AMyPawn_JumpTeleportMoveForward_Statics::Function_MetaDataParams, ARRAY_COUNT(Z_Construct_UFunction_AMyPawn_JumpTeleportMoveForward_Statics::Function_MetaDataParams)) };
+	UFunction* Z_Construct_UFunction_AMyPawn_JumpTeleportMoveForward()
+	{
+		static UFunction* ReturnFunction = nullptr;
+		if (!ReturnFunction)
+		{
+			UE4CodeGen_Private::ConstructUFunction(ReturnFunction, Z_Construct_UFunction_AMyPawn_JumpTeleportMoveForward_Statics::FuncParams);
+		}
+		return ReturnFunction;
+	}
+	UClass* Z_Construct_UClass_AMyPawn_NoRegister()
+	{
+		return AMyPawn::StaticClass();
+	}
+	struct Z_Construct_UClass_AMyPawn_Statics
+	{
+		static UObject* (*const DependentSingletons[])();
+		static const FClassFunctionLinkInfo FuncInfo[];
+#if WITH_METADATA
+		static const UE4CodeGen_Private::FMetaDataPairParam Class_MetaDataParams[];
+#endif
+		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
+		static const UE4CodeGen_Private::FClassParams ClassParams;
+	};
+	UObject* (*const Z_Construct_UClass_AMyPawn_Statics::DependentSingletons[])() = {
+		(UObject* (*)())Z_Construct_UClass_APawn,
+		(UObject* (*)())Z_Construct_UPackage__Script_Project1,
+	};
+	const FClassFunctionLinkInfo Z_Construct_UClass_AMyPawn_Statics::FuncInfo[] = {
+		{ &Z_Construct_UFunction_AMyPawn_JumpTeleportMoveForward, "JumpTeleportMoveForward" }, // 2125415650
+	};
+#if WITH_METADATA
+	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_AMyPawn_Statics::Class_MetaDataParams[] = {
+		{ "HideCategories", "Navigation" },
+		{ "IncludePath", "MyPawn.h" },
+		{ "ModuleRelativePath", "MyPawn.h" },
+	};
+#endif
+	const FCppClassTypeInfoStatic Z_Construct_UClass_AMyPawn_Statics::StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<AMyPawn>::IsAbstract,
+	};
+	const UE4CodeGen_Private::FClassParams Z_Construct_UClass_AMyPawn_Statics::ClassParams = {
+		&AMyPawn::StaticClass,
+		DependentSingletons, ARRAY_COUNT(DependentSingletons),
+		0x009000A0u,
+		FuncInfo, ARRAY_COUNT(FuncInfo),
+		nullptr, 0,
+		nullptr,
+		&StaticCppClassTypeInfo,
+		nullptr, 0,
+		METADATA_PARAMS(Z_Construct_UClass_AMyPawn_Statics::Class_MetaDataParams, ARRAY_COUNT(Z_Construct_UClass_AMyPawn_Statics::Class_MetaDataParams))
+	};
+	UClass* Z_Construct_UClass_AMyPawn()
+	{
+		static UClass* OuterClass = nullptr;
+		if (!OuterClass)
+		{
+			UE4CodeGen_Private::ConstructUClass(OuterClass, Z_Construct_UClass_AMyPawn_Statics::ClassParams);
+		}
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(AMyPawn, 348357917);
+	static FCompiledInDefer Z_CompiledInDefer_UClass_AMyPawn(Z_Construct_UClass_AMyPawn, &AMyPawn::StaticClass, TEXT("/Script/Project1"), TEXT("AMyPawn"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(AMyPawn);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
